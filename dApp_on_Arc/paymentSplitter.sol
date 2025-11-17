d6dd8c44e4659afc87af2444d79c510da4cc52f7
Emmanuel-webDev <emmanuelonuorah875@gmail.com>
2025-11-17 10:27:56 +0100

    Added payment splitter

--- dApp_on_Arc/paymentSplitter.sol
+++ dApp_on_Arc/paymentSplitter.sol
@@ -11,6 +11,7 @@ contract Payer {
     IERC20 public immutable token =
         IERC20(0x3600000000000000000000000000000000000000); // USDC token address for arc
     mapping(address => uint) public userBalance;
+    uint public amountToSplit;
 
     //Deposit to contract
     function deposit(uint _amount) public payable {
@@ -32,5 +33,40 @@ contract Payer {
         userBalance[msg.sender] -= _amount;
 
         token.safeTransfer(msg.sender, _amount);
+    }
+
+    //Funds splitter
+    function splitFunds(
+        address payable[] memory recipents,
+        uint32[] memory percents
+    ) public payable {
+        require(userBalance[msg.sender] > 0, "Insufficient balance to split");
+        require(
+            recipents.length == percents.length,
+            "Recipents and percents must be the same length"
+        );
+        require(recipents.length > 0, "Recipents must be greater than 0");
+        require(percents.length > 0, "Percents must be greater than 0");
+
+        uint32 totalPercent = 0;
+        for (uint i = 0; i < percents.length; i++) {
+            totalPercent += percents[i];
+        }
+        require(
+            totalPercent <= 100,
+            "Total percent must be less than or equal to 100"
+        );
+
+        amountToSplit = (userBalance[msg.sender] * totalPercent) / 100;
+        userBalance[msg.sender] -= amountToSplit;
+
+        //Fund distributor loop
+        for (uint i = 0; i <= recipents.length; i++) {
+            uint256 amount = (userBalance[msg.sender] * percents[i]) / 100;
+
+            if (amount > 0) {
+                token.safeTransfer(recipents[i], amount);
+            }
+        }
     }
 }
